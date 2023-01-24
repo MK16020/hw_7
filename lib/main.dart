5416db1a576e88a46c70770dd5e62df82078b2e2
MK16020 <61877101+MK16020@users.noreply.github.com>
2023-01-24 09:10:29 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hw_7/pages/firstPage.dart';
+import 'package:hw_7/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: FirstPage());
+    return const MaterialApp(home: HomePage());
   }
 }
